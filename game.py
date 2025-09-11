1bb05ec8e4ead44a331493b43c984238f1451d92
Dysfunctional-Human <sskaarav@gmail.com>
2025-09-11 23:19:48 +0530

    finalized game

--- game.py
+++ game.py
@@ -53,68 +53,67 @@ def play_game():
     """Starts the Flappy Bird game and keeps it running till bird either goes out of bounds or collides with a pipe
     """
     
-    global WIN
-    win = WIN
+    global WIN      # Using global game window
     
-    birds = []
-    birds.append(Bird(230, 350))
+    birds = []    # List of alive birds
+    birds.append(Bird(230, 350))   # Initializing bird object
     
-    base = Base(FLOOR)
-    pipes = [Pipe(700)]
-    score = 0
+    base = Base(FLOOR)   # Initializing Base object
+    pipes = [Pipe(700)]  # Initializing Pipe object
+    score = 0   # Current score
     
-    clock = pygame.time.Clock()
+    clock = pygame.time.Clock()   # Game clock
     
     run = True
-    while run and len(birds) > 0:
-        clock.tick(30)
+    while run and len(birds) > 0:   # While the bird hasn't collided and there is atleast one bird alive
+        clock.tick(30)   # Running the game at 30 FPS
         
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pygame.event.get():    # Quit the game if window is closed
+            if event.type == pygame.QUIT:   
                 run = False
                 pygame.quit()
                 quit()
                 break
             
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN:   # make the bird jump if spacebar is pressed
                 if event.key == pygame.K_SPACE:
                     for bird in birds:
                         bird.jump()
         
-        for bird in birds:
-            bird.move()
+        for bird in birds:   # Move the bird(s)
+            bird.move()  
         
-        base.move()
+        base.move()  # Move the pipe
         
         rem = []
-        add_pipe = False
+        add_pipe = False    # Flag to check if new pipe needs to be added
         for pipe in pipes:
-            pipe.move()
+            pipe.move()     # Move all pipes on screen
             
             for bird in birds:
-                if pipe.collide(bird=bird):
+                if pipe.collide(bird=bird):   # Check for bird-pipe collision
                     birds.pop(birds.index(bird))
             
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
                 rem.append(pipe)
             
-            if not pipe.passed and pipe.x < bird.x:
+            if not pipe.passed and pipe.x < bird.x:   # if bird has passed the pipe, update 
                 pipe.passed = True
                 add_pipe = True
         
         if add_pipe:
-            score += 1
+            score += 1    # increment score if bird has passed a pipe
             
-            pipes.append(Pipe(WIN_WIDTH))
+            pipes.append(Pipe(WIN_WIDTH))     
         
         for r in rem:
-            pipes.remove(r)
+            pipes.remove(r)   
         
         for bird in birds:
             if bird.y + bird.img.get_height() - 10 >= FLOOR or bird.y < -50:
                 birds.pop(birds.index(bird))
         
-        draw_game_window(win=WIN, birds=birds, pipes=pipes, base=base, score=score)
+        draw_game_window(win=WIN, birds=birds, pipes=pipes, base=base, score=score) # rendering game window
 
 if __name__ == '__main__':
     play_game()        
